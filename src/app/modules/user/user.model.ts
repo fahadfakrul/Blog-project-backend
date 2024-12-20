8e25eab6ea1e73d3e73b65b4c46904c6dc8c6085
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-12-20 18:30:28 +0600

    user model created

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      validate: {
+        validator: function (value: string) {
+          return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(value);
+        },
+        message: '{VALUE} is not a valid email',
+      },
+      immutable: true,
+      unique: true,
+    },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'user'] },
+    isBlocked: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const User = model<TUser>('User', userSchema);
