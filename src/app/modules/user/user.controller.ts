9b021cfde5ebaf2cc15c45bf9bcb72538aeef14e
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-12-21 23:28:30 +0600

    send respons utility created successfully

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserServices } from './user.service';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userDetails = req.body;
     const result = await UserServices.createUserIntoDB(userDetails);
-    res.status(201).json({
+    sendResponse(res, {
       success: true,
+      statusCode: httpStatus.CREATED,
       message: 'User registered successfully',
       data: result,
     });
