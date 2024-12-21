39cf5e7cc42500a599f1cd5a19dca9b36fe54539
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-12-21 20:15:32 +0600

    bcrypt encription added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,6 +1,7 @@
 import { model, Schema } from 'mongoose';
 import { TUser } from './user.interface';
-
+import bcrypt from 'bcrypt';
+import config from '../../config';
 const userSchema = new Schema<TUser>(
   {
     name: { type: String, required: true },
@@ -16,5 +17,22 @@ const userSchema = new Schema<TUser>(
   },
   { timestamps: true }
 );
+
+// pre save middleware/hook
+userSchema.pre('save', async function (next) {
+  //hashing password
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
+
+// post save middleware/hook
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
 
 export const User = model<TUser>('User', userSchema);
