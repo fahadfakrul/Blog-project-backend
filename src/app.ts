711718afd6e51aa84d3b144857501b8eaa1c9051
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-12-22 08:42:41 +0600

    router redundance handled

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 const app: Application = express();
 const port = 3000;
 
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use('/api/auth', UserRoutes);
+app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is running!');
 });
